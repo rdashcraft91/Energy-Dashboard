2660a979b4d94ccb485b51671718e8383f41799a
Ryan Ashcraft <rashcraft1991@gmail.com>
2020-06-11 22:00:22 -0500

    mongo atlas updates

--- try.py
+++ try.py
@@ -2,10 +2,9 @@
 from flask import Flask, jsonify, render_template
 import json
 from pymongo import MongoClient
-from mongo_conn import mongo_pass
 
 # setup mongo connection
-conn = f"mongodb+srv://rdashcraft91:{mongo_pass}@cluster0-qoy1h.mongodb.net/test"
+conn = "mongodb+srv://rdashcraft91:R4%24hcr4ft13@cluster0-qoy1h.mongodb.net/test"
 client = MongoClient(conn)
 
 
@@ -14,7 +13,7 @@ client = MongoClient(conn)
 #################################################
 # connect to mongo db and collection
 db = client.eia_db
-total_energy = db.total_energy
+state_energy = db.state_collection
 
 
 #################################################
@@ -40,14 +39,14 @@ def show_apis():
     """List all available api routes."""
     return (
         f"<h4>Available Routes:</h4>"
-        f'<a href="/api/v1.0/total_energy">/api/v1.0/total_energy</a><br/>'       
+        f'<a href="/api/v1.0/state_energy">/api/v1.0/state_energy</a><br/>'       
 
     )    
 
-@app.route("/api/v1.0/total_energy")
-def get_total_energy():
-    # data = total_energy.find_one('consumption') # To select one id of data
-    data = list(total_energy.find())  # To look at all data
+@app.route("/api/v1.0/state_energy")
+def get_state_energy():
+    # data = state_energy.find_one('consumption') # To select one id of data
+    data = list(state_energy.find())  # To look at all data
     return jsonify(data)
 
 # @app.route("/api/v1.0/info")
@@ -56,6 +55,7 @@ def get_total_energy():
 
 # @app.route("/api/v1.0/info/<subject_id>")
 # def get_one_user_results(subject_id):
+#     data = list(state_energy.find())
 #     return jsonify(data.get_data_by_user(subject_id))    
 
 # @app.route("/api/v1.0/subjects")
