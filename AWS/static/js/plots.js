f13da020b4785798b0abcb300ed272480a612a59
Ryan Ashcraft <rashcraft1991@gmail.com>
2020-06-13 17:39:25 -0500

    state sunburst

--- AWS/static/js/plots.js
+++ AWS/static/js/plots.js
@@ -124,6 +124,121 @@ function buildTotalEnergyPie(dataset, keys, titlePie) {
 
 getTotalEnergyData();
 
+function getStateSunburstData() {
+  var dataUrl = '/api/v1.0/state_energy';
+
+  console.log(dataUrl);
+
+  Plotly.d3.json(dataUrl, function(err, data){
+  
+    console.log(data);
+
+    delete data[0]._id;
+    delete data[1]._id;
+
+    console.log(data[0]);
+
+    var consumption_data = data[0];
+
+    var consumption_entries = Object.entries(consumption_data);
+
+    console.log(consumption_entries);
+
+    var commonSource = []
+
+    consumption_entries.forEach(function(state){
+        
+        console.log(state);
+
+        console.log(state[1]);
+        var sourceStates = state[1];
+        console.log(sourceStates);
+        var maxSource = Object.keys(sourceStates).reduce((a, b) => sourceStates[a] > sourceStates[b] ? a : b);
+        console.log(`${state[0]}: ${maxSource}`);
+        var new_result = {};
+        new_result[`${state[0]}`] = maxSource
+        new_result['BTU'] = sourceStates[`${maxSource}`]
+        commonSource.push(new_result);
+      });
+    
+    console.log(commonSource);
+
+    var labels = []
+    var parents = []
+    var values = []
+    labels.push("Renewable Sources");
+
+    var bioTotal = 0;
+    var hydroTotal = 0;
+    var geoTotal = 0;
+    var solarTotal = 0;
+    var windTotal = 0;
+
+    commonSource.forEach(function(result) {
+      if (Object.values(result)[0] === 'Bio') {
+        bioTotal += Object.values(result)[1];
+      }
+      else if (Object.values(result)[0] === 'Hydro') {
+        hydroTotal += Object.values(result)[1];
+      }
+      else if (Object.values(result)[0] === 'Geo') {
+        geoTotal += Object.values(result)[1];
+      }
+      else if (Object.values(result)[0] === 'Solar') {
+        solarTotal += Object.values(result)[1];
+      }
+      else {
+        windTotal += Object.values(result)[1];
+      }
+    })
+    
+    commonSource.forEach(function (result){
+      if (labels.includes(Object.values(result)[0])) {
+        
+      }
+      else {
+        labels.push(Object.values(result)[0]);
+      }
+    });
+
+    parents.push("", "Renewable Sources", "Renewable Sources", "Renewable Sources", "Renewable Sources");
+    values.push("", hydroTotal, bioTotal, solarTotal, windTotal);
+
+    commonSource.forEach(function(result) {
+          
+      labels.push(Object.keys(result)[0]);
+      parents.push(Object.values(result)[0]);
+      values.push(Object.values(result)[1]);
+        
+    });
+    
+    console.log(labels, parents, values)
+    
+    var data = [
+      {
+        "type": "sunburst",
+        "labels": labels,
+        "parents": parents,
+        "values":  values,
+        "leaf": {"opacity": 0.4},
+        "marker": {"line": {"width": 2}},
+        "branchvalues": 'total',
+        "insidetextorientation": "horizontal"
+      }];  
+    var layout = {
+      "margin": {"l": 0, "r": 0, "b": 0, "t": 50},
+      title: "Most Used Renewable Energy Source by State (BTU)"
+    };
+
+    Plotly.newPlot('totalStateplot', data, layout, {showSendToCloud: true})
+        
+    // myPlot = document.getElementById("plot");
+    
+  });
+}
+
+getStateSunburstData();
+
 
 // // Initialize the dashboard
 // init();
